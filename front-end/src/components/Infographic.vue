bc1f9f73cc3e5c2a11669881c29bf8b4e05f3dfb
Rui Neves <ruijsneves@gmail.com>
2022-06-19 22:28:22 +0100

    inicio da infografia

--- front-end/src/components/Infographic.vue
+++ front-end/src/components/Infographic.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="infographic">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis sem non
-      ipsum tempor sollicitudin at sed justo. Suspendisse lobortis turpis purus,
-      in mattis turpis consequat nec. Sed sodales orci vitae hendrerit rutrum.
-      Donec luctus dui augue, quis bibendum libero ultrices ultricies. Donec vel
-      dignissim augue, eget mollis lectus. Class aptent taciti sociosqu ad
-      litora torquent per conubia nostra, per inceptos himenaeos. Aenean leo
-      enim, commodo dignissim urna faucibus, commodo dapibus nisl. Sed pretium
-      ligula erat, non porta justo mollis quis. Duis pellentesque consequat
-      velit non blandit.
-    </p>
+    <div class="dados">
+      <section class="info small">
+        <p>Books</p>
+        <p>
+          {{ data.numberBooks }}
+        </p>
+      </section>
+      <section class="info med">
+        <p>Movies</p>
+        <p>
+          {{ data.numberMovies }}
+        </p>
+      </section>
+      <section class="info big">
+        <p>Characters</p>
+        <p>
+          {{ data.numberCharacters }}
+        </p>
+      </section>
+    </div>
+    <p>This infographic was obtained at: {{ data.createAt }}</p>
+    <img
+      alt="Delete this infographic"
+      title="Delete this infographic"
+      @click="delete data._id"
+      :src="`${publicPath}imgs/delete.png`"
+    />
   </div>
 </template>
 
@@ -20,16 +36,64 @@ export default {
   props: {
     data: { type: Object, required: true },
   },
+  methods: {
+    delete(id) {
+      console.log(id);
+    },
+  },
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+    };
+  },
 };
 </script>
 
 <style scoped>
 .infographic {
-  background-color: #a18a43;
+  background-color: #9f6319;
   color: #fff;
   width: 70%;
   margin: auto;
   padding: 30px;
   border-radius: 50px;
+  padding-bottom: 50px;
+}
+
+.dados {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.info {
+  border: 1px solid #dbc023;
+  color: #dbc023;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.small {
+  width: 80px;
+  height: 80px;
+}
+
+.med {
+  width: 120px;
+  height: 120px;
+}
+
+.big {
+  width: 200px;
+  height: 200px;
+}
+img {
+  width: 30px;
+  float: right;
+  cursor: pointer;
 }
 </style>
--- front-end/src/views/InfographicsView.vue
+++ front-end/src/views/InfographicsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="infographics" :class="{ 'side-padding': listVisible }">
     <info-list @changed="listChanged" />
-    <app-infographic />
+    <app-infographic :data="mock" />
     <!-- <div>
       <p>
         <span>Note:</span>This information is relative only to the original Lord
@@ -24,6 +24,98 @@ export default {
   data() {
     return {
       listVisible: true,
+      mock: {
+        _id: "fdkjgdflkjgklsçjkflºk",
+        createAt: "18-06-2022 09:07:59",
+        numberBooks: 5,
+        numberMovies: 5,
+        numberCharacters: 20,
+        numberQuotes: 9999999,
+        books: [
+          {
+            name: "The Fellowship Of The Ring",
+            chapters: ["a", "b", "c"],
+          },
+          {
+            name: "The Fellowship Of The Ring 2",
+            chapters: ["a", "b", "c"],
+          },
+        ],
+        movies: [
+          {
+            name: "The Lord of the Rings Series",
+            runtimeInMinutes: 558,
+            budgetInMillions: 281,
+            boxOfficeRevenueInMillions: 2917,
+            academyAwardNominations: 30,
+            academyAwardWins: 17,
+            rottenTomatoesScore: 94,
+          },
+          {
+            name: "The Lord of the Rings Series 2",
+            runtimeInMinutes: 558,
+            budgetInMillions: 281,
+            boxOfficeRevenueInMillions: 2917,
+            academyAwardNominations: 30,
+            academyAwardWins: 17,
+            rottenTomatoesScore: 94,
+          },
+        ],
+        characters: [
+          {
+            Human: [
+              {
+                name: "",
+                birth: 6,
+                quotes: [],
+              },
+              {
+                name: "",
+                birth: 6,
+                quotes: [],
+              },
+            ],
+            Elf: [
+              {
+                name: "",
+                birth: 6,
+              },
+              {
+                name: "",
+                birth: 6,
+              },
+            ],
+          },
+        ],
+        mostTalkativeCharacters: [
+          {
+            name: "Tyrion Lannister",
+            quotes: 46556,
+          },
+          {
+            name: "Jon Sun",
+            quotes: 3655,
+          },
+          {
+            name: "Cercei Lannister",
+            quotes: 344,
+          },
+          {
+            name: "Dany Targ",
+            quotes: 56,
+          },
+        ],
+        mostSuccessMovies: [
+          {
+            name: "movie name",
+            ratio: 1.6,
+          },
+          {
+            name: "movie name 2",
+            ratio: 1.3,
+          },
+        ],
+      },
     };
   },
   methods: {
