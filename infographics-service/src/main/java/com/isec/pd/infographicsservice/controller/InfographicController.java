d0ae90138c51ba4613399b2f9b5ef021354f783d
Leandro José <a21260032@alunos.isec.pt>
2022-06-20 22:22:34 +0100

    Infographic Service: Fix error 'No Access-Control-Allow-Origin'

--- infographics-service/src/main/java/com/isec/pd/infographicsservice/controller/InfographicController.java
+++ infographics-service/src/main/java/com/isec/pd/infographicsservice/controller/InfographicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8082", maxAge = 3600) // Required to solve the error: "has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource"
 @RequestMapping("/infographic")
 public class InfographicController {
 
